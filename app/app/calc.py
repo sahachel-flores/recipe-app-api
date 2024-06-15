017cb70e06a6f73a90b761ebd3ac46742dc1e402
sahachel-flores <sahachel.j.flores@gmail.com>
2024-06-14 18:45:00 -0700

    adding test files  and database

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculate function
+"""
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    """ Sub x from y and return result """
+    return 0
---
+++ app/app/test.py
@@ -0,0 +1,18 @@
+"""
+Sample test
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    """ Test calc module """
+    def test_add_numers(self):
+        res = calc.add(5,6)
+        self.assertEqual(res,11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, 5)
